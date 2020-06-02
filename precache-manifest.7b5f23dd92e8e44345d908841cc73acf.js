8ab84002d3c581711276c59a5c231239d35876d0
Charles Ivia <iviacharles@gmail.com>
2020-06-02 18:25:33 +0300

    Updates

---
+++ precache-manifest.7b5f23dd92e8e44345d908841cc73acf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698b62af52c37d90e435b2bc5a641dd1",
+    "url": "/CharlesIvia/expense-tracker/index.html"
+  },
+  {
+    "revision": "8eed16beaf9d5d19970a",
+    "url": "/CharlesIvia/expense-tracker/static/css/main.6b74f544.chunk.css"
+  },
+  {
+    "revision": "27f1e8b4c27101a2c40b",
+    "url": "/CharlesIvia/expense-tracker/static/js/2.96c65e06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CharlesIvia/expense-tracker/static/js/2.96c65e06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8eed16beaf9d5d19970a",
+    "url": "/CharlesIvia/expense-tracker/static/js/main.fda4756e.chunk.js"
+  },
+  {
+    "revision": "87e0ecd1f7eeba7c3b30",
+    "url": "/CharlesIvia/expense-tracker/static/js/runtime-main.fdf15b5e.js"
+  }
+]);
